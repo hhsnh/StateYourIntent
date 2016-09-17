27d8ac0983f49577eb9733ccd899ff38c83787b3
yanhao Wang <hhsnh2014@gmail.com>
2016-09-17 16:09:20 +0800

    headfirst 第二章

---
+++ app/src/main/java/com/example/yanhaowang/stateyourintent/ReceiveMessageActivity.java
@@ -0,0 +1,37 @@
+package com.example.yanhaowang.stateyourintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String messageText= intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
